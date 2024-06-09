24d3652d58d15d2d3f8da46b98756fe1e7ccad6c
Emmexgold <Arubiemmanuel803@gmail.com>
2024-06-09 19:04:36 +0100

    payment gateway

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("payment gateway"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
